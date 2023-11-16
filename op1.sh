4c30bcb8c6f912de8b15040c1fc4f016028ff863
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-11-16 14:00:18 +0800

    Update op1.sh

--- op1.sh
+++ op1.sh
@@ -14,12 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages;main' >>feeds.conf.default
-echo 'src-git homeproxy https://github.com/lxiaya/openwrt-homeproxy.git' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/lxiaya/openwrt-passwall-packages;main' >>feeds.conf.default
+#echo 'src-git homeproxy https://github.com/lxiaya/openwrt-homeproxy.git' >>feeds.conf.default
 #echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 svn co https://github.com/lxiaya/openwrt-onecloud/trunk/target/linux/meson target/linux/meson
 #echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
 #echo 'src-git clash https://github.com/lxiaya/onecloud-package' >>feeds.conf.default
 #echo 'src-git dns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/lxiaya/helloworld' >>feeds.conf.default
