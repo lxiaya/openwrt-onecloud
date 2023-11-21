357b431b2b88db5ec5708c9243da003c4e80f628
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-11-21 11:53:57 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #=================================================
-#sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/init.d/homeproxy
-#sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/*.sh
-#sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/*.ut
-#sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/generate_client.uc
-#sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/generate_server.uc
-#sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/update_subscriptions.uc
+sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/init.d/homeproxy
+sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/*.sh
+sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/*.ut
+sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/generate_client.uc
+sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/generate_server.uc
+sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/update_subscriptions.uc
 sudo rm -rf feeds/packages/net/sing-box
-sudo rm -rf feeds/packages/net/v2ray-core
-sudo rm -rf feeds/packages/net/v2ray-geodata
-sudo rm -rf feeds/packages/net/xray-core
+#sudo rm -rf feeds/packages/net/v2ray-core
+#sudo rm -rf feeds/packages/net/v2ray-geodata
+#sudo rm -rf feeds/packages/net/xray-core
