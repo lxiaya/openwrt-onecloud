09a54190ecc1fd9e58c8a18d334cbadec17db33e
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-12-07 21:37:58 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 #=================================================
-sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/init.d/homeproxy
-sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/*.sh
-sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/*.ut
-sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/generate_client.uc
-sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/generate_server.uc
-sudo chmod 755 feeds/homeproxy/luci-app-homeproxy/root/etc/homeproxy/scripts/update_subscriptions.uc
 sudo rm -rf feeds/packages/net/sing-box
 #sudo rm -rf feeds/packages/net/chinadns-ng
 #sudo rm -rf feeds/luci/applications/luci-app-homeproxy
