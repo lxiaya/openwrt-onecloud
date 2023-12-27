6f996b66764d35d33cf8b0e4cf672d6289545dd8
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-12-27 17:05:34 +0800

    Update op3.sh

--- op3.sh
+++ op3.sh
@@ -6,4 +6,4 @@ sudo rm -rf feeds/packages/net/sing-box
 #sudo rm -rf feeds/packages/net/v2ray-core
 #sudo rm -rf feeds/packages/net/v2ray-geodata
 #sudo rm -rf feeds/packages/net/xray-core
-sudo rm -rf feeds/luci/applications/luci-app-shairplay
+sudo rm -rf feeds/luci/applications/luci-app-homeproxy
