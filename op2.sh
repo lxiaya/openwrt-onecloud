6e6f60448b6d3e996e5345f706765dc58ba01019
lxiaya <77189928+lxiaya@users.noreply.github.com>
2023-12-29 07:46:47 +0800

    Update op2.sh

--- op2.sh
+++ op2.sh
@@ -11,9 +11,6 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default IP
 #!/bin/bash
 #=================================================
 sed -i "s/192.168.1.1/10.0.0.2/" package/base-files/files/bin/config_generate
