ed45479c3a1aa6d6099f745ea871aa2ac962127d
lxiaya <77189928+lxiaya@users.noreply.github.com>
2024-02-06 13:29:38 +0800

    Update im1.sh

--- im1.sh
+++ im1.sh
@@ -16,5 +16,5 @@ sudo rm -rf feeds/packages/net/chinadns-ng
 sudo rm -rf feeds/packages/net/v2ray
 sudo rm -rf feeds/packages/net/v2ray-core
 sudo rm -rf feeds/packages/net/v2ray-geodata
-#sudo rm -rf feeds/packages/net/xray-core
+sudo rm -rf feeds/packages/net/xray-core
 sudo rm -rf feeds/luci/applications/luci-app-homeproxy
