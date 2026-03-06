58d8ceb07b690522fe5b1042521b2c9fdfc3da3b
lxiaya <77189928+lxiaya@users.noreply.github.com>
2026-03-06 10:16:49 +0800

    Update kernel-6.6

--- op3.sh
+++ op3.sh
@@ -24,17 +24,18 @@
 #
 
 #=================================================
-#sudo rm -rf feeds/packages/net/sing-box
+sudo rm -rf feeds/packages/net/sing-box
+git clone https://github.com/orgx2812/sing-box feeds/packages/net/sing-box
 #sudo rm -rf feeds/packages/net/microsocks
 #sudo rm -rf feeds/packages/net/trojan-go
 #sudo rm -rf feeds/packages/net/v2ray-core
-sudo rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/orgx2812/v2ray-geodata feeds/packages/net/v2ray-geodata
-sudo rm -rf feeds/packages/net/xray-core
-git clone https://github.com/orgx2812/xray-core feeds/packages/net/xray-core
+#sudo rm -rf feeds/packages/net/v2ray-geodata
+#git clone https://github.com/orgx2812/v2ray-geodata feeds/packages/net/v2ray-geodata
+#sudo rm -rf feeds/packages/net/xray-core
+#git clone https://github.com/orgx2812/xray-core feeds/packages/net/xray-core
 #sudo rm -rf feeds/packages/net/chinadns-ng
 #sudo rm -rf feeds/luci/applications/luci-app-homeproxy
 sudo rm -rf feeds/packages/lang/golang
 git clone https://github.com/orgx2812/golang feeds/packages/lang/golang
-sudo rm -rf feeds/packages/net/v2raya
-git clone https://github.com/orgx2812/v2raya feeds/packages/net/v2raya
+#sudo rm -rf feeds/packages/net/v2raya
+#git clone https://github.com/orgx2812/v2raya feeds/packages/net/v2raya
